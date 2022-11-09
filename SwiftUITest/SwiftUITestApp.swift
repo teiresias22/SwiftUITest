c96d1a9adab55bbe32f62e4e98552d88daa5eb6c
teiresias22 <teiresias1987@gmail.com>
2022-11-09 19:31:59 +0900

    Initial Commit

---
+++ SwiftUITest/SwiftUITestApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUITestApp.swift
+//  SwiftUITest
+//
+//  Created by Joonhwan Jeon on 2022/11/09.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
