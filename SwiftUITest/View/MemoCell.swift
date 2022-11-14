0cb247feb1eced30e1db2067520111cee041f96d
teiresias22 <teiresias1987@gmail.com>
2022-11-14 21:35:22 +0900

    MemoCell 분리

---
+++ SwiftUITest/View/MemoCell.swift
@@ -0,0 +1,30 @@
+//
+//  MemoCell.swift
+//  SwiftUITest
+//
+//  Created by Joonhwan Jeon on 2022/11/14.
+//
+
+import SwiftUI
+
+struct MemoCell: View {
+    @ObservedObject var memo: Memo
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(memo.content)
+                .font(.body)
+                .lineLimit(1)
+            
+            Text(memo.insertDate, style: .date)
+                .font(.caption)
+                .foregroundColor(.secondary)
+        }
+    }
+}
+
+struct MemoCell_Previews: PreviewProvider {
+    static var previews: some View {
+        MemoCell(memo: Memo(content: "Test"))
+    }
+}
